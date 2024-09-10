301bee8591b54e20662a1d046779c30bc4a108b6
mo-han-raj <56388701+mo-han-raj@users.noreply.github.com>
2024-09-10 14:26:10 +0200

    Update ThemeProvider.tsx
    no need

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,15 +1,1 @@
-'use client';
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { ThemeProviderProps } from 'next-themes/dist/types';
-import { ReactNode } from 'react';
-
-interface Props extends ThemeProviderProps {
-    children: ReactNode;
-}
-
-export function ThemeProvider ({children, ...props}:ThemeProviderProps){
-    return <NextThemesProvider {...props}>{children}
-
-    </NextThemesProvider>
-}
